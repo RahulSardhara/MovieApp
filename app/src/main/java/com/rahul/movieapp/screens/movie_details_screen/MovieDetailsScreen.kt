0e03553ac4d4dd87a45bee00192eef1673bc7bc5
Rahul Sardhara <rahul.sardhara07@gmail.com>
2025-06-30 21:54:02 +0530

    Movie Details Screen has been added..

--- app/src/main/java/com/rahul/movieapp/screens/movie_details_screen/MovieDetailsScreen.kt
+++ app/src/main/java/com/rahul/movieapp/screens/movie_details_screen/MovieDetailsScreen.kt
@@ -2,15 +2,16 @@ package com.rahul.movieapp.screens.movie_details_screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -23,20 +24,25 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import com.rahul.movieapp.model.Movie
 import com.rahul.movieapp.model.getMovies
+import com.rahul.movieapp.widgets.MovieRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieDetailScreen(navController: NavController, movieName: String) {
 
-    val movie = getMovies().find { it.id == movieName }
+    val movie : Movie? = getMovies().firstOrNull { it.id == movieName }
 
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                title = { Text(movieName) },
+                title = { Text(movie?.title?:"") },
                 navigationIcon = {
                     IconButton(onClick = {
                         navController.popBackStack()
@@ -56,18 +62,39 @@ fun MovieDetailScreen(navController: NavController, movieName: String) {
                 .fillMaxWidth()
                 .fillMaxHeight()
         ) {
-            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("Movie Details Screen $movieName")
-                Spacer(modifier = Modifier.height(23.dp))
-                Button(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Text("Go Back")
+            Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
+                movie?.let {
+                    MovieRow(movieName = movie)
+                    Text(it.title, style = MaterialTheme.typography.headlineLarge)
+                    HorizontalScrollableImageView(it)
                 }
+
+
             }
         }
 
 
     }
 
+}
+
+@Composable
+private fun HorizontalScrollableImageView(movie1: Movie) {
+    LazyRow {
+        items(movie1.images) { image ->
+            Card(modifier = Modifier
+                .padding(12.dp)
+                .size(240.dp), elevation = CardDefaults.cardElevation(5.dp)) {
+                AsyncImage(
+                    model = image,
+                    contentDescription = "Movie Image",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(240.dp)
+                        .clip(MaterialTheme.shapes.large),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
 }
